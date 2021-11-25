04c71c55e2549c6085d28e312adebb4f8f55772f
Jamesson Vinicius <j.0vinicius@hotmail.com>
2021-11-25 08:31:22 -0300

    Subtotal & Total methods

--- src/main/java/com/senac/projeto/entities/Order.java
+++ src/main/java/com/senac/projeto/entities/Order.java
@@ -89,13 +89,6 @@ public class Order implements Serializable{
 		this.client = client;
 	}
 	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
 	
 	public Payment getPayment() {
 		return payment;
@@ -109,6 +102,22 @@ public class Order implements Serializable{
 		return items;
 	}
 	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
